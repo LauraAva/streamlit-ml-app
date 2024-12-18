b479872e99688794ffc75ff4a4b3a2a5e8bb5fcd
LauraAva <laura.demiri1003@gmail.com>
2024-12-18 20:41:11 +0100

    Update Modeling.py

--- pages/Modeling.py
+++ pages/Modeling.py
@@ -67,7 +67,7 @@ if model_choice == "Random Forest":
     model = RandomForestClassifier(n_estimators=50, max_depth=10, random_state=42)
     if st.checkbox("Enable Hyperparameter Tuning"):
         param_grid = {"n_estimators": [50, 100, 200], "max_depth": [10, 20, None]}
-        grid_search = GridSearchCV(model, param_grid, cv=3, , scoring="accuracy")
+        grid_search = GridSearchCV(model, param_grid, cv=3, scoring="accuracy")
         grid_search.fit(X_train, y_train)
         model = grid_search.best_estimator_
         st.write(f"Best Parameters: {grid_search.best_params_}")
